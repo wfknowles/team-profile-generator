a7f645c628bc43ce455d3a6bc383ec1e33ba73e0
Will Knowles <will@willknowles.com>
2021-08-26 11:05:50 -0500

    employee tests all failing

---
+++ __tests__/employee.tests.js
@@ -0,0 +1,34 @@
+const { expect } = require('@jest/globals');
+const Employee = require('../lib/Employee.js');
+
+test('creates an employee object', () => {
+    let employee = new Employee('Dwight Schrute','dwight@dwightschrute.com');
+
+    expect(employee.name).toBe('Dwight Schrute');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('dwight@dwightschrute.com');
+    expect(employee.role).toBe('Employee');
+});
+
+test('get name from employee.getName()', () => {
+    let employee = new Employee('Dwight Schrute', 'dwight@dwightschrute.com');
+    expect(employee.getName()).toBe(employee.name);
+});
+
+test('get id from employee.getID()', () => {
+    let employee = new Employee('Dwight Schrute', 'dwight@dwightschrute.com');
+    expect(employee.getId()).toBe(employee.id);
+});
+
+test('get email from employee.getEmail()', () => {
+    let employee = new Employee('Dwight Schrute', 'dwight@dwightschrute.com');
+
+    expect(employee.getEmail()).toBe(employee.email);
+
+});
+
+test('get role from employee.getRole()', () => {
+    let employee = new Employee('Dwight Schrute', 'dwight@dwightschrute.com');
+    expect(employee.getRole()).toBe('Employee');
+});
+
---
+++ index.js
@@ -0,0 +1,33 @@
+/* 
+Generator() => {
+    * team = []
+    * getTeam(), start(), finish(), add(), build()
+}
+
+Employee() => {
+    * Name, ID, Email
+    * getName(), getId(), getEmail(), getRole() // Returns 'Employee'
+    * validate?
+    * tests
+}
+
+Manager() => {
+    * officeNumber
+    * getRole() // Overridden to return 'Manager'
+    * validate?
+}
+
+Engineer() => {
+    * github
+    * getGithub(), getRole() // Overridden to return 'Engineer'
+    * validate?
+    * tests
+}
+
+Intern() => {
+    * school
+    * getSchool(), getRole() // Overridden to return 'Intern'
+    * validate?
+    * tests
+}
+*/
