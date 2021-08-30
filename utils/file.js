82836c3ebba1b0fd0a448818c9883f3ed3bb4994
Will Knowles <will@willknowles.com>
2021-08-30 14:47:02 -0500

    updated writeFile to create dist folder

--- utils/file.js
+++ utils/file.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
+        // If the 'dist' folder doesn't exist, create it...
+        if (!fs.existsSync('./dist')) {
+            fs.mkdirSync('./dist');
+        }
+
         fs.writeFile('./dist/index.html', fileContent, err => {
             if (err) {
                 reject(err);
